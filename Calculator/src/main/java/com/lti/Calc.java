8d9c9665b1604c9cb2edae25e1ca8a9e8e93f8ea
Vishaha Ram S <vishak.28@gmail.com>
2020-12-21 15:02:05 +0530

    diff commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,9 +4,14 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
+	
+	public static int sub(int num1,int num2) {
+		return num1-num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(10,20));
+		System.out.println("Sum="+add(10,20));
+		System.out.println("Difference="+sub(90,20));
 	}
 
 }
